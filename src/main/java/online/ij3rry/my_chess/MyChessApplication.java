2e1e691c4c46aef4525f1ea01911f530db194506
Jerin Johnykutty <36392038+J3rryCodes@users.noreply.github.com>
2022-11-19 23:29:03 +0530

    template

---
+++ src/main/java/online/ij3rry/my_chess/MyChessApplication.java
@@ -0,0 +1,13 @@
+package online.ij3rry.my_chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyChessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyChessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/ij3rry/my_chess/MyChessApplicationTests.java
@@ -0,0 +1,13 @@
+package online.ij3rry.my_chess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyChessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
